03e518dcc2ddddd0e5b8ebd07aac546ee6142df3
YashwanthRadhakrishnan <120356954+YashwanthRadhakrishnan@users.noreply.github.com>
2022-12-12 11:22:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+const myStyle1={
+  backgroundImage:
+  "url('pic1.webp')",
+  height: '500px',
+  width:'1519px',
+  marginTop:'10px',
+  marginBottom:'10px',
+  backgroundSize:'cover',
+  backgroundRepeat:'no-repeat',
+}
+return(
+  <div>
+  <div><br></br><br></br><br></br><br></br>
+  <div style={myStyle1}></div>
